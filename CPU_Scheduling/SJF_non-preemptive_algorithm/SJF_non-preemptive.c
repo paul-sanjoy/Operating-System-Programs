0d695300563322f551030080d26003dd5ce6f580
paul-sanjoy <sanjoyworkspace@gmail.com>
2022-01-17 23:41:11 +0530

    added function for calculations

--- CPU_Scheduling/SJF_non-preemptive_algorithm/SJF_non-preemptive.c
+++ CPU_Scheduling/SJF_non-preemptive_algorithm/SJF_non-preemptive.c
@@ -30,3 +30,44 @@ void sortArrivalTime(int n, int p_details[][6])
         }
     }
 }
+
+void timeCalculations(int n, int p_details[][6])
+{
+    int index ,i , j, k, temp;
+    int prevCompTime;	//to store the completion time of previous process
+    int currBurstTime;	//to store the minimum burst time of any process whose arrival time is less than or equal to prevCompTime 
+    
+	/* for the 1st process */ 
+    p_details[0][3] = p_details[0][1] + p_details[0][2]; //completion time = arrival time + burst time
+    p_details[0][5] = p_details[0][3] - p_details[0][1]; //turn around time = completion time - arrival time
+    p_details[0][4] = p_details[0][5] - p_details[0][2]; //waiting time = turn around time - burst time
+    
+	/* from 2nd process onwards */ 
+    for( i=1; i<n; i++)
+    {
+        prevCompTime = p_details[i-1][3]; 
+        currBurstTime = p_details[i][2];
+        for(j=i; j<n; j++)
+        {
+        	//if arrival time of jth process is less or equal to prev Completion Time
+        	//and burst time of jth process is less or equal to current Burst Time 
+            if(prevCompTime >= p_details[j][1] && currBurstTime >= p_details[j][2])
+            {
+                currBurstTime = p_details[j][2]; //update current burst time
+                index = j;
+            }
+        }
+        //now for 'index'th process--
+        p_details[index][3] = prevCompTime + p_details[index][2];	//completion time = arrival time + burst time
+        p_details[index][5] = p_details[index][3] - p_details[index][1];	//turn around time = completion time - arrival time
+        p_details[index][4] = p_details[index][5] - p_details[index][2];	//waiting time = turn around time - burst time
+        
+        //swapping all 6 process details of 'index'th process with ith process--
+        for(k=0; k<6; k++)
+        {
+            temp = p_details[index][k];		
+            p_details[index][k] = p_details[i][k];
+            p_details[i][k] = temp;
+        }
+    }
+}
