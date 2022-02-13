693561cefe69192f517bd13a35e062aa6ac537e5
paul-sanjoy <sanjoyworkspace@gmail.com>
2022-02-13 09:55:30 +0530

    Update Best-fit.c

--- Memory_Management/Best-fit_algorithm/Best-fit.c
+++ Memory_Management/Best-fit_algorithm/Best-fit.c
@@ -18,4 +18,36 @@ typedef struct Process
 	int allocated;
 }Process;
 
-
+int main()
+{
+	int nb,i,np,j,lowest,index ;
+	
+	//Input the memory block partitions:
+	printf("Enter the number of memory blocks: ");
+	scanf("%d",&nb);
+	
+	mBlock blck[nb];
+	
+	printf("Enter the sizes of the memory blocks: ");
+	for(i=0; i<nb ; i++)
+	{
+		blck[i].id = i+1;
+		scanf("%d",&(blck[i].unused));
+		blck[i].used = 0; //initially used memory is zero
+	}
+	
+	//Input the processes:
+	printf("\nEnter the number of processes: ");
+	scanf("%d",&np);
+	
+	Process proc[np];
+	
+	printf("Enter the sizes of the processes: ");
+	for(i=0; i<np ; i++)
+	{
+		proc[i].id = i+1;
+		scanf("%d",&(proc[i].size));
+		proc[i].allocated = 0; //initially process is not allocated
+	}
+	
+	
