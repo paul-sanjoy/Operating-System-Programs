f4c25b8d61d76a89e731e18f2d1adc2277677684
paul-sanjoy <sanjoyworkspace@gmail.com>
2022-02-13 09:55:00 +0530

    Create Best-fit.c

---
+++ Memory_Management/Best-fit_algorithm/Best-fit.c
@@ -0,0 +1,21 @@
+
+
+#include<stdio.h>
+
+//structure for memory block
+typedef struct mBlock
+{
+	int id;
+	int unused;
+	int used;
+}mBlock;
+
+//structure for process
+typedef struct Process
+{
+	int id;
+	int size;
+	int allocated;
+}Process;
+
+
