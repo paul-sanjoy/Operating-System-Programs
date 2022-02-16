4af3f2bde442827b5fd68e4d021e98e80af9e4c1
paul-sanjoy <sanjoyworkspace@gmail.com>
2022-02-16 09:35:29 +0530

    Update Find_most_efficient.c

--- Memory_Management/Find_most_efficient_algorithm/Find_most_efficient.c
+++ Memory_Management/Find_most_efficient_algorithm/Find_most_efficient.c
@@ -25,3 +25,40 @@ typedef struct Process
 }Process;
 
 
+/*first fit algorithm*/
+int firstFit(mBlock blck[],Process proc[], int nb ,int np,int *num)
+{
+	int i , j ,totalFrag=0;
+	
+	for(i=0;i<np;i++)
+	{
+		for(j=0;j<nb;j++)
+		{
+			if(blck[j].unused >= proc[i].size)
+			{
+				blck[j].unused -= proc[i].size; //unused memory after process allocation
+				blck[j].used += proc[i].size; //used memory after process allocation
+				proc[i].allocated = 1;
+				break;
+			}
+			
+		}
+		
+		if(proc[i].allocated == 0)  //no. of not allocated processes 
+		{
+			(*num)++ ;
+		}
+	}
+	
+	for(i=0; i<=nb;i++ ) //calculate total fragmentation
+	{
+		if(blck[i].used != 0)
+			totalFrag += blck[i].unused;
+	}
+	
+	return totalFrag;
+	
+}
+
+
+
