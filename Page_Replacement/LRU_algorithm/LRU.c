c4c2dfb10d4806d32c8744577b985c3c2e325f08
paul-sanjoy <sanjoyworkspace@gmail.com>
2022-03-06 18:36:51 +0530

    Update LRU.c

--- Page_Replacement/LRU_algorithm/LRU.c
+++ Page_Replacement/LRU_algorithm/LRU.c
@@ -27,3 +27,32 @@ void printTable(int framesTable[], int numFrames)
 			printf("\t%d",framesTable[i]);	
 	}
 }
+
+
+int main()
+{
+	int i,j,k,numFrames, n, numFaults=0 , pos=0, current, lastPos[10], flag=0, min, lruIndex;
+	
+	printf("Enter the number of page frames: ");
+	scanf("%d",&numFrames);
+	
+	int framesTable[numFrames];
+	for(i=0;i<numFrames;i++)
+	{
+		framesTable[i] = -1;
+	}
+	
+	printf("Enter the total number of page request: ");	
+	scanf("%d", &n);	
+	int ref_seq[n];
+	
+	printf("Enter the reference sequence: ");	
+	for(i=0;i<n;i++)
+	{
+		scanf("%d", &ref_seq[i]);
+	}
+		
+	printf("\nPage frame status after each page request: ");
+	printf("\nPage Reference\t\tHit/Miss\tPage Frame Status\n");
+	
+	
