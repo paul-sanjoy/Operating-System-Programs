58492facce570e261ae8dda4e0b00676eb145978
paul-sanjoy <sanjoyworkspace@gmail.com>
2022-03-13 22:45:05 +0530

    Create Optimal.c

---
+++ Page_Replacement/Optimal_algorithm/Optimal.c
@@ -0,0 +1,29 @@
+
+
+#include<stdio.h>
+
+//This checks whether the page is already present
+int isPresent(int framesTable[], int numFrames, int pg_req )
+{
+	int i;
+	for(i=0; i< numFrames; i++)
+	{
+		if(framesTable[i] == pg_req)
+			return 1;
+	}
+	
+	return 0;	
+}
+
+//This prints page frame status
+void printTable(int framesTable[], int numFrames)
+{
+	int i;
+	for(i=0; i<numFrames; i++)
+	{
+		if(framesTable[i] == -1)
+			printf("\tempty");
+		else
+			printf("\t%d",framesTable[i]);	
+	}
+}
