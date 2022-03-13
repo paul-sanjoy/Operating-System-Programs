2b9f66c93552d6938185e14d85fc21297788e4d5
paul-sanjoy <sanjoyworkspace@gmail.com>
2022-03-13 22:46:47 +0530

    Update Optimal.c

--- Page_Replacement/Optimal_algorithm/Optimal.c
+++ Page_Replacement/Optimal_algorithm/Optimal.c
@@ -1,4 +1,4 @@
-
+/*   Optimal page replacement   */
 
 #include<stdio.h>
 
@@ -55,4 +55,47 @@ int main()
 	printf("\nPage frame status after each page request: ");
 	printf("\nPage Reference\t\tHit/Miss\tPage Frame Status\n");
 	
+	/*calculation*/
+	for(i=0; i<n; i++)
+	{
+		printf("\n  %d",ref_seq[i]);
+		if(!isPresent(framesTable, numFrames, ref_seq[i])) //if the requested page is present
+		{
+			printf("\t\t\tMiss\t ");
+			for(j=0; j<numFrames; j++)
+			{
+				current = framesTable[j];	//one of current pages in table
+				for(k = i+1; k<n ; k++)		//check the future pages in sequence
+				{
+					if(current == ref_seq[k]) //when current page is found among future pages in seq.
+					{
+						futurePos[j] = k; //store the index
+						break;
+					}
+					else		//if current page is not found in future seq
+						futurePos[j] = 99999;	//this initialization is important
+				}
+			}
+			max = -999;
+			for(j=0; j<numFrames; j++)
+			{
+				if(futurePos[j] > max)  //check the furthest from the indexes
+				{
+					max = futurePos[j];
+					optIndex = j;   	//store the index of furthest
+				}
+			}
+			framesTable[optIndex] = ref_seq[i];	//replace the page which is furthest in future of seq.
+			numFaults++;
+			
+			printTable(framesTable, numFrames);
+			
+		}
+		else
+			printf("\t\t\tHit\t ");
+	}
+	printf("\n\nTotal number of page faults: %d", numFaults);
 	
+	return 0;
+	
+}
